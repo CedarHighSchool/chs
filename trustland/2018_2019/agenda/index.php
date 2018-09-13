7225752a871b8d22a426ffab60b52fbcb1db3868
caswallow <caleb.swallow@ironmail.org>
2018-09-13 07:43:47 -0600

    update trustland

---
+++ trustland/2018_2019/agenda/index.php
@@ -0,0 +1,79 @@
+<?php $thisPage="registration"; ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+
+		<title>Agenda's/Minutes 2018-2019</title>
+
+    	<?php include("./../../../assets/inc/headtag.php") ?>
+
+  	</head>
+
+<body>
+
+		<?php include("./../../../assets/inc/navbar.php") ?>
+
+		
+
+    	<!-- Let the container begin! -->
+
+		<div class="container">
+
+			<div class="row">
+
+				
+
+				<?php include("./../../../assets/inc/navlist.php") ?>
+
+				
+
+	   			<div class="span9">		
+
+	       			<div class="well">
+
+          				<h3 align="center"><a href="../../index.php">School Land Trust 2018-2019</a></h3>
+
+       			  </div>
+
+         		</div>
+
+		           	
+
+		        <br />
+
+				
+
+				<div class="span9">
+
+<div class="well" align=center>
+				
+            <p>&nbsp;</p>
+            <h3>Agenda</h3><br>
+              
+  
+                <br>
+                <h3>Minutes</h3>
+                
+                <br>   
+
+    <!--<p<a href=""> Minutes (Draft) 1-17-17</a> <br>-->
+    <!--<a href=""> Minutes Approved 1-17-17</a>    -->
+    <!--<p<a href=""> Minutes (Draft) 2-7-17</a> <br> -->
+    <!--  <a href=""> Minutes Approved 2-7-17</a <br>-->
+      
+    <!--<p <a href=""> Minutes (Draft) 3-14-17</a><br> -->
+    <!--                <a href=""> Minutes Approved 3-14-17</a> <br> -->
+                    
+    <!--<p <a href=""> Minutes (Draft) 4-11-17</a> <br> -->
+    <!--                <a href=""> Minutes Approved 4-11-17</a> <br> -->
+                
+
+  <p>&nbsp;</p>
+</div>
+</div>
+
+			    	<p><br />
+  </p>
