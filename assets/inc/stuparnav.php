f7785fa5acc52a3898fc2646752afa18f6a7bea4
ethan.hunt <eth189hunt@gmail.com>
2018-09-28 08:24:58 -0600

    Database edit for resources

--- assets/inc/stuparnav.php
+++ assets/inc/stuparnav.php
@@ -2,44 +2,7 @@
 
 	<div class="well">
 
-   		<h5>Resources</h5>
-
-   		<ul class="nav nav-list">
-
-			<li><a href="https://irondistrict.org/media/forms/District%20Calendars/Iron_County_School_District_Calendar_18-19_SUPER_HIGH_QUALITY.pdf">A / B Schedule</a></li>
-
-			<li><a href="/general/bell-schedule.php">Bell Schedule</a></li>
-            
-			<li><a href="/custodial.php">Custodial Survey</a></li>
-			<li><a href="/counseling/index.php">Counseling</a></li>
-			<li><a href="/courses/">Course Catalog</a></li>
-			<li><a href="https://docs.google.com/document/d/1OPKy50U5zjMKkaf9nc0Qt3pWB7rakmSpQIlARRCHMas/edit">Dances</a></li>
-            <li><a href="/general/guestpasspdf.pdf">Dance Guest Pass</a></li>
-			<li><a href="http://irondistrict.org/">District Office</a></li>
-			<li><a href="http://irondistrict.org/sites/default/files/Docs/1516FEESCHEDULE.pdf">Fee Schedule</a>
-            <li><a href="https://secureinstantpayments.com/sip/cart/event.php?EID=999">Fee Payment Online</a>
-            </li><li><a href="/counseling/Graduation Requirements/CHS Requirements.xls - Sheet1.pdf">Graduation Information</a></li>
-
-			<li><a href="http://irondistrict.org/sites/default/files/PDFs/Calendar%2015-16.pdf">Holiday Schedule</a>
-			
-			<li><a href="http://irondistrict.org/sites/default/files/secondarylunch.pdf">Lunch Menu</a></li>
-			<li><a href="https://www.myschoolbucks.com/index.jsp">Lunch Online Payment</a></li>
-			<li><a href="http://www.jostens.com/misc/aboutus/about_jostens_lp_main.html">Order a Yearbook</a></li>
-			<li><a href="/pfcc.php">PFCC/Council</a></li>
-            <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSeN3OTM2_tOVKG0FIGyJKdB6pJp-4KLfpjDMCaG27OulK9Xjw/viewform?usp=sf_link">Redmen Pride Nomination Form</a></li>  
- 			<li><a href="/student/redmenpride/index.php">Redmen Pride Winners</a></li>
- 			<li><a href="/registration.php">Registration</a></li>
-			<li><a href="https://docs.google.com/a/ironmail.org/spreadsheet/viewform?formkey=dDRQa0xaNXBVd3lBYktDa2FMNzYxS0E6MQ">Report Bullying</a></li>
-			<li><a href="/counseling/scholarship information/Scholarship.pdf">Scholarships</a></li>
-			<li><a href="/student/sterlingscholar">Sterling Scholars</a></li>
-			<li><a href="../../general/Handbook 2016-17.pdf"target="_blank">Student Handbook
-									</a></li>								
-<li><a href="https://images.jostens.com/login.jsp">Submit a Picture</a></li>	
-			<li><a href="/counseling/testing/">Testing Information</a></li>
-			<li><a href="/learningcenter.php">Learning Center</a></li>
-			<li><a href="http://www.skalooza.com/utah-iron-cedar_city/cedar_city_high/">View Yearbooks 1942-2007</a></li>
-			<li><a href="http://ps.irondistrict.org/public/">Update Parent / Guardian Info</a></li>
-		</ul>
+   		<?=area("stuparnav")?>
 
 	</div>
                     
