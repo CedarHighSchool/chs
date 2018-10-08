8886c0d6691347891f7a2f39f850e7e2a8c005a5
ethan.hunt <eth189hunt@gmail.com>
2018-10-08 08:44:59 -0600

    Add area for rest of bell schedules and add A day B day in better location

--- assets/inc/navlist.php
+++ assets/inc/navlist.php
@@ -22,6 +22,30 @@
 					<input type="text" name="q" id="q" placeholder="Search" value="<?=$_GET['q']?>" style="max-width:100%;"><input type="submit" value="Go" style="display:none;">
 				</form>
     </div>
+    <div class="well sidebar-nav">
+    	<?php
+      $key = "AIzaSyDhcx4hw1-HrEwIywR8Oc2QwOEeV7EqOAY";
+      $time_min = date("Y-m-d\TH:i:sP",strtotime(date("Y/m/d")));
+      $time_max = date("Y-m-d\TH:i:sP",strtotime("tomorrow"));
+      $maxRes = 1;
+      $url = "https://www.googleapis.com/calendar/v3/calendars/ironmail.org_dgnbo22qkr1htt9p91l54l1634@group.calendar.google.com/events?key=$key&maxResults=$maxRes&timeMin=$time_min&timeMax=$time_max&q=Day";
+      $res = file_get_contents($url);
+      $res = json_decode($res);
+      
+      foreach($res->items as $item) {
+          $test = $item->summary;
+          switch($test) {
+              case "A Day":
+                  $day = "A Day";
+                  break;
+              case "B Day";
+                  $day = "B Day";
+                  break;
+          }
+      }
+      ?>
+      <span><h1><?=$day?$day:""?></h1></span>
+    </div>
 
 	
 
