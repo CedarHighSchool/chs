e460c993496cf1fda0d0e9f830bf0b36167a42ac
caswallow <caleb.swallow@ironmail.org>
2018-10-25 09:04:50 -0600

    students page

---
+++ student.php
@@ -0,0 +1,254 @@
+<?php $thisPage="students"; ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+
+		<title>Cedar High School</title>
+
+    	<?php include("assets/inc/headtag.php") ?>
+		
+ 	</head>
+
+  	<body>
+<!--I AM A BLUBBERFISH -->
+		<?php include("assets/inc/navbar.php") ?>
+				
+        <div class="container">
+        
+        	<div class="row">
+            
+            	<?php include("assets/inc/navlist.php") ?>
+                
+				<div class="span9">		
+
+	       			<div class="well">
+
+          				<h3>Students</h3>
+
+           			</div><!--well-->
+
+         		</div><!--span9-->
+
+		<div class="span9">
+        
+        	<div id="myCarousel" class="carousel slide">
+                
+			           		<div class="carousel-inner">
+
+			              			<div class="item active"><img src="student/new/StudentPagePic1.jpg" alt="">
+		              			  	<div class="carousel-caption">
+		              			  	  <h4>Theater</h4>
+		              			  	</div>
+			              			</div><!--end of item-->
+
+	  								<div class="item"><img src="student/new/StudentPagePic2.jpg" alt="">
+									<div class="carousel-caption">
+									  <h4>Pep Band</h4></div>
+                                    </div><!--end of item-->
+
+           			  				<div class="item"><img src="student/new/StudentPagePic3.jpg" alt="">
+                                   	<div class="carousel-caption">
+                                   	  <h4>Cheer</h4></div>
+		              			  	</div><!--end of item-->
+
+           			  				<div class="item"><img src="student/new/StudentPagePic5.jpg" alt=""></a>
+                                    <div class="carousel-caption">
+                                      <h4>Football</h4> </div>
+									</div><!--end of item-->
+
+		            		  </div><!--inner carousel-->
+
+			            		<a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+
+			            		<a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
+				</div><!--end of slideshow-->
+                
+                <div class="span2"></div>
+
+	   		<div class="row-fluid">
+            
+            
+            <div class="span8">
+				<div class="well">
+					<?=area('student_officers');?>
+            	</div><!--well-->
+            
+                 
+				<div class="well">
+                          
+                	<h3><span class="style9">Redmen Pride Nominations</span><br></h3>
+                    
+                    <p>Submit Redmen Pride nominations online using the link below. Only seniors can be nominated.<br>
+                  </p>
+                          
+                   	<a href="https://docs.google.com/a/ironmail.org/forms/d/1IukIgxxmqY7-qt74baE53hPgpBTJ6J8kOeox-FCVb0I/edit"target="_blank"> Redmen Pride Nomination Form </a><br> 
+            	</div><!--well-->
+<div class="well">
+                      	      <h3>Registration Information</h3>
+                       	        <a href="general/12th Grade Course Request From.pdf"target="_blank">12th Grade Course Request Form</a> <br>
+                       	        <a href="general/11th Grade Course Request Form.pdf"target="_blank">11th Grade Course Request Form</a><br>
+                       	        <a href="general/10th Grade Course Request Form.pdf"target="_blank">10th Grade Course Request Form</a><br>
+                       	        <a href="general/9th Grade Course Request Form.pdf"target="_blank">9th Grade Course Request Form</a><br> 
+                              
+                       	       
+                   	       
+                                            
+                   	        <p><a href="https://docs.google.com/document/d/1NVlsfzdQ31lNtApkx8o5tELEXEfPjXeeM96mC5OI6Go/edit"target="_blank">Letter to parents</a> regarding college and career readiness (CCR) parent/teacher conferences <a href="https://drive.google.com/file/d/0B99_s7rAlqHFS3ZyaUlneW40Rk0/view?usp=sharing"target="_blank"><br>
+                   	          AUP Instructions for Electronic Signatures</a> -Student<br>
+                       	        <a href="https://drive.google.com/file/d/0B99_s7rAlqHFS3ZyaUlneW40Rk0/view?usp=sharing"target="_blank">AUP Instructions for Electronic Signatures</a> - Parent<br>
+                       	        <a href="https://drive.google.com/file/d/0B99_s7rAlqHFZWw1RUZYcUhyOGlfdkk2ZnljcG1uMEROUVU0/view?usp=sharing"target="_blank">Safe School Policy Instructions for Electronic Signatures</a> - Parents<br>
+                       	        <a href="../../general/Handbook 2016-17.pdf"target="_blank">Cedar High Handbook</a>                        	  <br>
+                       	        <a href="https://docs.google.com/a/ironmail.org/forms/d/e/1FAIpQLSc4y2f1RBE-v88n9_2MuZPV4nIGsF5G7pHDov3wD4nXdw635w/viewform"target="_blank">Student Handbook Review Sign-Off </a>                        	  <br><a href="https://www.youtube.com/watch?v=ZaVRucnaHRo"target="_blank">Show Safe UT video</a> (Juniors, Sophomores, Freshmen)<br>
+            </div><!--well-->
+           	  <div class="well">
+
+	       		  <h3><a name="actdates"/><a href="http://www.actstudent.org/regist/dates.html">ACT dates</a></h3>
+                              ACT Code 450-045<br>
+   				  </p>
+   				  <p><h4>NEXT YEAR DATES</h4></p>
+			    <table width="459" style="text-align:left">
+   				    <thead>
+   				      <tr>
+   				        <th width="118">Test Date</th>
+   				        <th width="145">Registration Deadline</th>
+   				        <th width="174">Late Fee Required</th>
+			          </tr>
+			        </thead>
+   				    <tbody>
+   				      <tr>
+   				        <td colspan="1" rowspan="1">September 8, 2018</td>
+   				        <td colspan="1" rowspan="1">August 12, 2018</td>
+   				        <td colspan="1" rowspan="1">August 26, 2018</td>
+   				       </tr>
+   				      <tr>
+   				        <td colspan="1" rowspan="1">October 27, 2018</td>
+   				        <td colspan="1" rowspan="1">September 28, 2018</td>
+   				        <td colspan="1" rowspan="1">October 14, 2018</td>
+			          </tr>
+   				      <tr>
+   				        <td colspan="1" rowspan="1">December 8, 2018</td>
+   				        <td colspan="1" rowspan="1">November 2, 2018</td>
+   				        <td colspan="1" rowspan="1">November 19, 2018</td>
+   				       </tr>
+   				      <tr>
+   				        <td colspan="1" rowspan="1">February 9, 2019 (Juniors)</td>
+   				        <td colspan="1" rowspan="1">January 11, 2019</td>
+   				        <td colspan="1" rowspan="1">January 18, 2019</td>
+			          </tr>
+   				      <tr>
+   				        <td colspan="1" rowspan="1">April 13, 2019</td>
+   				        <td colspan="1" rowspan="1">March 8, 2018</td>
+   				        <td colspan="1" rowspan="1">March 25, 2019</td>
+			          </tr>
+   				      <tr>
+   				        <td colspan="1" rowspan="1">June 8, 2019</td>
+   				        <td colspan="1" rowspan="1">May 3, 2019</td>
+   				        <td colspan="1" rowspan="1">May 20, 2019</td>
+			          </tr>
+   				      <tr>
+   				        <td colspan="1" rowspan="1">July 13, 2019</td>
+   				        <td colspan="1" rowspan="1">June 14, 2019</td>
+   				        <td colspan="1" rowspan="1">June 24, 2019</td>
+			          </tr>
+			        </tbody>
+			    </table>
+   				  <p><br>Click <a href="http://www.actstudent.org/regist/dates.html"target="_blank">here</a> for ACT website</p>
+           	  </div><!--well-->
+                
+                
+
+    <div class="well">
+    <h3>Summer Activities</h3>                      
+    <p>Not sure what you want to do next summer? Click the links below for different internships and summer camps!<br>
+    <a href="https://drive.google.com/file/d/0B5xgpm9L00dIcjdYSGhqNkQ5MmVrUDNacGVMNy0xRDA2d1Jn/view?usp=sharing">-Spy Hops Pops Brochure</a><br>
+    <a href="https://drive.google.com/file/d/0B5xgpm9L00dIODJnMm9welRNS2s0Y1pOelZ2OGVrbHNRRDRz/view?usp=sharing">-801 Sessions Internships</a><br>
+    <a href="https://drive.google.com/file/d/0B5xgpm9L00dITTA4SUlubDBxLW9NWkFWS3YtZGlhMzN2TGRr/view?usp=sharing">-Film, Audio or Design Internship Programs</a><br>
+    <a href="https://drive.google.com/file/d/0B5xgpm9L00dIZlA0MURZZHB2cjhWdW54Y3loSVdHTVdPdWZz/view?usp=sharing">-Spy Hops Summer Brochure Outside</a><br>
+    <a href="https://drive.google.com/file/d/0B5xgpm9L00dIRHl5eGNJNTJ5YjdKVWxUblhRVWlhZDlCaUJZ/view?usp=sharing">-Spy Hops Summber Brochure Inside</a>
+    </p>
+    </div>
+    
+    <div class="well">
+
+          				    <h3>Helpful Links</h3>
+
+		           				<p><a href="https://www.utips.org/#"target="_blank">Utips</a><br><a href="https://utahfutures.org/"target="_blank">Utah Futures</a><br>
+		           				  <a href="http://www.jostens.com/?gclid=CPCQj9XNrMoCFYqGfgodV20PEg">Jostens</a><br>
+		           				Contact Jostens to order caps and gowns, class rings, and letterman jackets.</p>
+   			  </div><!--well-->
+                  
+			  <div class="well">
+
+		          				<h3>Mrs.Stubbs Window Hours</h3>
+		          				<p><a href="https://secureinstantpayments.com/sip/cart/event.php?EID=999"target="_blank">Online Payment</a> </p>
+		          				<p>Tuesday<strong>-</strong>Friday 7:30am to 8:00am<br>
+		          				  Monday-Friday 11:00am to 12:30pm<br>
+		          				Monday-Friday 2:40pm to 3:15pm</p>
+   			  </div><!--well-->
+              </div><!--span8-->
+                        
+                        
+                       
+                        
+                        
+
+                        <!--student navlist-->
+                  
+                  		<?php include("assets/inc/stuparnav.php") ?><!--span4-->
+                        
+                         <div class="span4">
+                
+                	
+	        	
+	        	<?php if($GAME_UP == true) { ?>
+			    	<div class="well">
+
+			      		<h4>Hunt High Score</h4>
+			      		<?php
+					      $link = mysqli_connect("schoolsites","chscyberweb",$pwwd,"chs");
+						 		if(!$link){
+						 			echo mysqli_error($link);
+						 		}
+						 		$stuff = mysqli_query($link, "SELECT * FROM `game_users` WHERE `hidden`=0 ORDER BY `score` DESC, `level` DESC");
+						 		$item = mysqli_fetch_array($stuff);
+					      ?>
+					      <center>
+					      	<img src="https://api.adorable.io/avatars/285/<?=$item['username']?>.png" id="icon_img" style="max-width: 150px; border-radius: 30%;" class="img-circle">
+					      	<h3><?=$item['username']?></h3>
+				      	</center>
+                What's the Hunt? Look on the home page for the first clue!
+			    	</div>
+			    	<?php } ?>
+                
+       		</div>
+                 	</div><!--row-fluid-->
+
+			</div><!--span9-->
+            
+      	</div><!--row-->
+            
+            	<?php include("assets/inc/footer.php") ?>
+
+  	</div> <!-- /container -->
+    
+    	<!-- Yummy JavaScrips
+
+    	================================================== -->
+
+    	<!-- Placed at the end of the document so the pages load faster -->
+
+    	<script src="assets/js/jquery.js"></script>
+
+    	<script src="assets/js/bootstrap.js"></script>
+
+    	<script src="assets/js/bootstrap-carousel.js"></script>
+
+
+
+  	</body>
+
+</html>
+
