dc391ae4feb759a99c04dc7fb879a26234a0e9cd
Mark Mumford <mark.mumford@ironmail.org>
2018-10-29 08:25:15 -0600

    removed class questions

--- index.php
+++ index.php
@@ -127,7 +127,7 @@
                                 </center>
     						</div>
     						<?php } ?>
-					
+
 							<?php
 							$stuff = $mysqli->query("SELECT * FROM `homepage` WHERE `visible`=1 ORDER BY `order` ASC");
 							while($item = $stuff->fetch_array()){
@@ -140,6 +140,8 @@
                                 }
 								if($item['type'] == 0){
 							?>
+							   
+							    
 								<div class="well wow fadeInUp">
 									<?=$delLink?>
 									<h3><?=$item['title']?></h3>
@@ -179,13 +181,6 @@
                 <?php
                 }
                 ?>
-                <div class="well">
-                    <h1><strong>Class Questions</strong></h1>
-                    <p><a href="https://docs.google.com/forms/d/1ZGDArGT6JuWNDtA4jaSYEVPJDxfyn1_2r3QK56xISyg/viewform?edit_requested=true">Freshman Class Questions</a></p>
-                    <p><a href="https://docs.google.com/forms/d/1-QvFW8MeuqgGIiRtbVLSB1_45cy9zEuLYcpuCEPe7r4/viewform?edit_requested=true">Sophomore Class Questions</a></p>
-                    <p><a href="https://docs.google.com/forms/d/1VUBnZCrysOXllMOtuQAcISAtz6sA4QnjkcLVeSDUzz8/viewform?edit_requested=true">Junior Class Questions</a></p>
-                    <p><a href="https://docs.google.com/forms/d/1JWxQIBf-bRSDyi8Un3JEV8VGB6WthRrwStdvhEfyvKI/viewform?edit_requested=true">Senior Class Questions</a></p>
-                </div>
                 <div class="well">
                     <?=area("ccr_box")?>
                 </div>
