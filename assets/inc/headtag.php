fd67af6fc8d4ebe795ab360505e8717eee288ec8
caswallow <caleb.swallow@ironmail.org>
2018-12-05 14:21:57 -0700

    update

--- assets/inc/headtag.php
+++ assets/inc/headtag.php
@@ -574,11 +574,11 @@ if(isset($_SESSION['username']) && $manage->check_perm("chat")) {
 	setInterval(userupdate, 2000);
  });
 </script>
-<!-- Hotjar Tracking Code for https://s.codetasty.com/caswallow/mysandbox/10_les -->
+<!-- Hotjar Tracking Code for chs.ironk12.org -->
 <script>
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:1117127,hjsv:6};
+        h._hjSettings={hjid:587327,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
