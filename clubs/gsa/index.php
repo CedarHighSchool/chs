338aec6fb80d844fb718aef22fff00914c752cdd
caswallow <caleb.swallow@ironmail.org>
2019-01-16 12:29:33 -0700

    added gsa club

---
+++ clubs/gsa/index.php
@@ -0,0 +1,149 @@
+<?php $thisPage="clubs"; ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+
+		<title>Cedar High School</title>
+		<?php include("../../assets/inc/headtag.php");
+    	if(!isset($_SESSION)){
+    		session_start();
+    	}
+    	?>
+    	
+ </head>
+
+  	<body>
+
+		<?php include("../../assets/inc/navbar.php") ?>
+		
+		
+		
+		
+		
+		<div class="container">
+			
+			<div class="row">
+				
+				
+				
+				<?php include("../../assets/inc/navlist.php") ?>
+				
+				
+				
+	   			<div class="span9">		
+
+	       			<div class="well">
+
+          				<h3>Gender and Sexuality Alliance</h3>
+
+           			</div>
+
+         		</div>
+
+		           	
+
+		       <div class="span2">	
+
+					<div class="thumbnail">				      		
+
+						<img src="gsa logo.png" alt="thumbnail">					</div>
+
+			  </div><br />
+
+					  
+
+				<div class="span4">
+				
+				
+				
+
+				
+
+			    	<div class="well">
+
+			      		<h3>Club Information</h3>
+			      		
+			      		<p>The Gender and Sexuality Alliance strives to spread awareness and acceptance of the</br>
+			      		LGBTQ+ community as well as provide a safe space for identifying, questioning,</br>
+			      		and supporting students. Meetings consist of planing the month's event, showing </br>
+			      		support for other members, and making new friends and having fun.</p>
+			      		
+			      					    	</div>
+
+			    	<div class="well">
+
+			      		<h3>How to Join</h3>
+
+		      		  <p>Ask GSA government, talk to Mr. Chidester or Mrs. Umin, or show up to the meetings.</p>
+
+			    	</div>
+					
+					<div class="well">
+
+   		<h3>When we meet</h3>
+   		
+   				      		  <p>Meetings are at 3:00 p.m. on Mondays in Mr. Chidester's room (#507)</p>
+			    	</div>
+				
+					
+<div class="well">
+
+   		<h3>Meetings</h3>
+
+		      		  <p>To be announced.</p>
+
+			    	</div>
+				</div>
+
+		           			
+
+				<div class="span3">
+					<div class="well">
+						<h5>Advisor</h5>
+						<ul class="nav nav-list">
+							<li><a href="../../faculty/Chidester-Johnathan/index.php">Mr. Chidester</a></li>	
+							<li><a href="../../faculty/uminski-brittany/index.php">Mrs. Uminski</a></li>
+				 		</ul>
+				 	</div>
+				  <div class="well">
+					<h5>Club Presindency</h5>
+					  <ul class="nav nav-list">
+						  <li>
+						    <div align="left">Presidents- <br><strong>Terrell Tom</strong><br><strong>Rebecca Mack</strong></div>
+						  </li>
+						  <li>
+						  	<div align="left">Historian- <br><strong>Serenity Braun</strong></div>
+						  </li>
+						  
+					  </ul>
+					</div>
+				</div>
+
+	       
+
+      		<?php include("../../assets/inc/footer.php") ?>
+
+
+
+    	</div> <!-- /container -->
+        
+
+
+    	<!-- Yummy JavaScrips
+
+    	================================================== -->
+
+    	<!-- Placed at the end of the document so the pages load faster -->
+
+    	<script src="/assets/js/jquery.js"></script>
+
+    	<script src="/assets/js/bootstrap.js"></script>
+
+
+
+  	</body>
+
+</html>
