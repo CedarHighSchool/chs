5f128d78e17fb500ce043dcaf2a6d14b20490952
caswallow <caleb.swallow@ironmail.org>
2019-02-08 08:52:41 -0700

    word

--- directory/pageforthatonelady.php
+++ directory/pageforthatonelady.php
@@ -1,57 +1,145 @@
-<?php $thisPage="Keur Knewmen"; ?>
+<?php $thisPage="Kaer Neumann"; ?>
+
 <!DOCTYPE html>
+
 <html lang="en">
+
 	<head>
+
 		<title>Cedar High School</title>
-    	<?php include("../assets/inc/headtag.php") ?>
+
+    	<?php include("../../assets/inc/headtag.php") ?>
+
   	</head>
+
   	<body>
-		<?php include("../assets/inc/navbar.php") ?>
+
+		<?php include("../../assets/inc/navbar.php") ?>
+
 		
+
     	<!-- Let the container begin! -->
+
 		<div class="container">
+
 			<div class="row">
-			  <?php include("../assets/inc/navlist.php") ?>
-			  <div class="span9">		
-	       				<div class="well">
-		          				<h3 align="center">Kaer Neumann</h3>
-          				  <p align="center">
-		           				<p align="center"><a href="https://docs.google.com/document/d/1bK4MON-YZC1Wyi1p6dwiHUoq0u83mLxlb6u7JndgY_M/edit?usp=sharing"> ACT Coach</a></p>
-		       			  </div>
-	   			</div><!--/span9 -->
-	</div><!--/row -->
-	<div class="container">
-	<li class="span2">	
+
+				
+
+				<?php include("../../assets/inc/navlist.php") ?>
+
+				<div class="span9">		
+
+	       			<div class="well">
+
+          				<h3><a href="./">ACT Test</a></h3>
+
+           			</div>
+
+         		</div>
+
+
+
+		         <div class="span2">	
 
 					<div class="thumbnail">	
 
-						<img src="fake-debbie.jpg"  alt="" />		
-						
-						</div>
+						<img src="fake-allson.jpg" alt="">
 
-			  </li><br />
-	</div>
-	<div class="container">
-			<div class="row">
-					<div class="span4">
-							<div class="well">
-								<h3 align="center">ACT Information</h3>
-								<p align="center">insert information here</p>
-							</div>
-				</div><!--/span4-->
+					</div>
+
+				</div><br/>
+
+				
+
+				<div class="span4">
+
+			    	<div class="well">
+
+			      		<h3>Test Information</h3>
+
+			      		<p><a href="http://www.thespectrum.com/section/HSSPORTS">High School Articles</a></p>
+
+	      		  </div>
+<div class="well">
+ 	<h3><span class="style9">ACT Prep</span></h3>
+    	<p>Sign up?</p>
+        </div>   
+			    	<div class="well">
+
+			      		<h3>About</h3>
+
+			      		<p>
+			      			Sign up for ACT prep during XLT in Fall, Winter, and Spring.			      		</p>
+
+		    	  </div>
+
+				</div>
+
+		           			
+<!-- lol -->
+				<div class="span3">
+
+					<div class="well">
+
+						<h5>Pages</h5>
+
+						<ul class="nav nav-list">
+
+							<li class="active"><a href="./">Home</a></li>
+
+							<li><a href="https://drive.google.com/file/d/1vjNq42mkZqNT7j2ISkYOjua9po0xPmLx/view?usp=sharing">Schedule</a></li>
+
+						  <li><a href="https://docs.google.com/a/ironmail.org/document/d/1OWeQMDNrrC_H1TSjK0_kQ8knHnUA24cGfEBf-Dbvqb8/edit">Varsity Roster</a></li>
+                          <li><a href="https://docs.google.com/a/ironmail.org/document/d/1U3fpX1P2UaYE30CuX7wO2gKHXMxB0GNqDY8yBzD9Z_Q/edit">JV Roster</a></li>
+						  <li><a href="https://docs.google.com/a/ironmail.org/document/d/1upeJtctUSZdwHAuvVEbSdeitpZlVvd32bNP8kYHTuQo/edit">Freshmen Roster</a></li>
+						  <li><a href="http://www.uhsaa.org/baseball/">UHSAA Baseball</a></li>
+
+						  <li><a href="gallery.php">Pictures</a></li>		
+
+				 		</ul>
+
+				 	</div>
+
+                    <div class="well">
+
+						<h5>Coaches</h5>
+
+						<ul class="nav nav-list">
+                        <li>Eric Fieldsted</li>
+
+					<li><a href="mailto:eric.fieldsted@ironmail.org">eric.fieldsted@ironmail.org</a></li>
+						</ul>
+
+			 	  </div>
+
+				</div>
+
 			</div><!--/row-->
-	</div>
 
-      		<?php include("../assets/inc/footer.php") ?>
+	       
 
-</div> <!-- /container -->
+      		<?php include("../../assets/inc/footer.php") ?>
+
+
+
+    	</div> <!-- /container -->
+
+
 
     	<!-- Yummy JavaScrips
+
     	================================================== -->
+
     	<!-- Placed at the end of the document so the pages load faster -->
+
     	<script src="/assets/js/jquery.js"></script>
+
     	<script src="/assets/js/bootstrap.js"></script>
-    	<script src="/assets/js/bootstrap-carousel.js"></script>
+
+
 
   	</body>
+
 </html>
+
