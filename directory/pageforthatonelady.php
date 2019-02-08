811ccd6435be1463396e6f8a7844ab6d78fa7a6a
caswallow <caleb.swallow@ironmail.org>
2019-02-08 10:27:15 -0700

    updated page

--- directory/pageforthatonelady.php
+++ directory/pageforthatonelady.php
@@ -8,13 +8,13 @@
 
 		<title>Cedar High School</title>
 
-    	<?php include("../../assets/inc/headtag.php") ?>
+    	<?php include("../assets/inc/headtag.php") ?>
 
   	</head>
 
   	<body>
 
-		<?php include("../../assets/inc/navbar.php") ?>
+		<?php include("../assets/inc/navbar.php") ?>
 
 		
 
@@ -26,13 +26,13 @@
 
 				
 
-				<?php include("../../assets/inc/navlist.php") ?>
+				<?php include("../assets/inc/navlist.php") ?>
 
 				<div class="span9">		
 
 	       			<div class="well">
 
-          				<h3><a href="./">ACT Test</a></h3>
+          				<h3 align="center"><a href="">ACT Testing Prep</a></h3>
 
            			</div>
 
@@ -44,7 +44,7 @@
 
 					<div class="thumbnail">	
 
-						<img src="fake-allson.jpg" alt="">
+						<img src="(../assets/img/neumann.jpg)" alt="">
 
 					</div>
 
@@ -58,7 +58,6 @@
 
 			      		<h3>Test Information</h3>
 
-			      		<p><a href="http://www.thespectrum.com/section/HSSPORTS">High School Articles</a></p>
 
 	      		  </div>
 <div class="well">
@@ -86,16 +85,16 @@
 
 						<ul class="nav nav-list">
 
-							<li class="active"><a href="./">Home</a></li>
+							
 
-							<li><a href="https://drive.google.com/file/d/1vjNq42mkZqNT7j2ISkYOjua9po0xPmLx/view?usp=sharing">Schedule</a></li>
+							<li><a href="">Schedule</a></li>
 
-						  <li><a href="https://docs.google.com/a/ironmail.org/document/d/1OWeQMDNrrC_H1TSjK0_kQ8knHnUA24cGfEBf-Dbvqb8/edit">Varsity Roster</a></li>
-                          <li><a href="https://docs.google.com/a/ironmail.org/document/d/1U3fpX1P2UaYE30CuX7wO2gKHXMxB0GNqDY8yBzD9Z_Q/edit">JV Roster</a></li>
-						  <li><a href="https://docs.google.com/a/ironmail.org/document/d/1upeJtctUSZdwHAuvVEbSdeitpZlVvd32bNP8kYHTuQo/edit">Freshmen Roster</a></li>
-						  <li><a href="http://www.uhsaa.org/baseball/">UHSAA Baseball</a></li>
+						  <li><a href="https://docs.google.com/document/d/1x3zeU7Zr23af3IhZajra662pExJGN0ud5UdYsoOCcMc/edit?usp=sharing">ACT Page</a></li>
+                          <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSdM7kcsMHgxeC3vRXI0yKSYucd7Ed2KixKlYlj7mT15qW16IA/viewform?usp=sf_link">XLT Tagging</a></li>
+						  <li><a href=""></a></li>
+						  <li><a href=""></a></li>
 
-						  <li><a href="gallery.php">Pictures</a></li>		
+						  	
 
 				 		</ul>
 
@@ -103,12 +102,12 @@
 
                     <div class="well">
 
-						<h5>Coaches</h5>
+						<h5>Coache</h5>
 
 						<ul class="nav nav-list">
-                        <li>Eric Fieldsted</li>
+                        <li>Kaer Neumann</li>
 
-					<li><a href="mailto:eric.fieldsted@ironmail.org">eric.fieldsted@ironmail.org</a></li>
+					<li><a href="mailto:kaer.neumann@ironmail.org">kaer.neumann@ironmail.org</a></li>
 						</ul>
 
 			 	  </div>
@@ -119,7 +118,7 @@
 
 	       
 
-      		<?php include("../../assets/inc/footer.php") ?>
+      		<?php include("../assets/inc/footer.php") ?>
 
 
 
