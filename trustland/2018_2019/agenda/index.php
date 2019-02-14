e352e58f1b8509ea2f76b68dd2bd47908df34e10
Mark Mumford <mark.mumford@ironmail.org>
2019-02-14 07:58:45 -0700

    added new trustland stuffs

--- trustland/2018_2019/agenda/index.php
+++ trustland/2018_2019/agenda/index.php
@@ -53,11 +53,14 @@
             <p>&nbsp;</p>
             <h3>Agenda</h3><br>
               <a href = "https://drive.google.com/open?id=0B9jFGSe1Qy6_cEhmd0IwaF9hdTFJeElmQnZWWEZvLTFYa1NV">Community Council Agenda 9-18-18</a>
+              <a href = "https://drive.google.com/open?id=11HWTXMkEk5LaUaMapgynL9KpNvFPnx5O">Community Council Agenda 11-27-18</a>
   
                 <br>
                 <h3>Minutes</h3>
                 <a href = "https://drive.google.com/open?id=0B9jFGSe1Qy6_bGhkLW0tcE0zeXh0RkRmdjBBWXpHUjJMTUUw">Minutes (Draft) 9-18-18</a><br>
                 <a href = "https://drive.google.com/open?id=0B9jFGSe1Qy6_eE9jaHBYdHUxQ3BsaHI2N2xzdTVWVGxWYVJV">Minutes Approved 9-18-18</a><br>
+                <a href = "https://drive.google.com/open?id=1zrHGubQuDzf-nHKxwEwlpDhIMDUfhdJC">Minutes (Draft) 11-27-18</a><br>
+                <a href = "https://drive.google.com/open?id=1whRonprgJEEOIYqmf_09zbsb0ni4fEoX">Minutes Approved 11-27-18</a><br>
                 <br>   
                 
 
