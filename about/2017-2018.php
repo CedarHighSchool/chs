0fae855245b91900ccf71ef6ac5f1e8a993d2d9e
Mark Mumford <mark.mumford@ironmail.org>
2019-02-25 09:25:30 -0700

    Added untracked files.

---
+++ about/2017-2018.php
@@ -0,0 +1,187 @@
+<?php $thisPage="about"; ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+
+		<title>Cedar High School</title>
+
+    	<?php include("../assets/inc/headtag.php") ?><style type="text/css">
+<!--
+.style3 {
+	font-family: inherit;
+	font-size: 18px;
+	color: #990000;
+}
+.style5 {font-family: inherit; font-size: 18px; color: #990000; font-weight: bold; }
+.style9 {color: #990000}
+-->
+        </style></head>
+
+  	<body>
+
+		<?php include("../assets/inc/navbar.php") ?>
+
+		
+
+    	<!-- Let the container begin! -->
+
+		<div class="container">
+
+			<div class="row">
+
+				
+
+				<?php include("../assets/inc/navlist.php") ?>
+                
+				<div class="span9">		
+
+	       			<div class="well">
+
+          				<h3>History</h3>
+
+           			</div>
+
+         		</div>
+                
+	   			<div class="span6">
+
+       			  <div class="well">
+                  
+       				<h3>2017 - 2018</h3>
+                    
+                    <p>
+                    <center>
+                    
+                    <h3><span class="style9">Class Officers</span><br></h3>
+                  <p><strong>Senior Officers:</strong><br>
+                  President - Lexi <br>
+                  Vice President - McClain <br>
+                  Secretary - Dream<br>
+                  Activities - Aspen
+                  <p><strong>Junior Officers:</strong><br>
+                  President - Sadie <br>
+                  Vice President - Kate <br>
+                  Secretary - Eliza <br>
+                  Activities - Russell
+                  <p><strong>Sophmore Officers:</strong><br>
+                  President - Coby <br>
+                  Vice President - Regan <br>
+                  Secretary - Kaycie <br>
+                  Activities - Jessica
+                  <p><strong>Freshmen Officers:</strong><br>
+                  President - Kailey<br>Vice President - Caleb<br>Secretary - Aubrey<br>Activities - Leilani</p></p></p>
+
+                      
+                      
+                      <hr></hr>
+                   
+                        <h3>Graduation Pictures - Historian Video</h3>
+                    
+
+<hr>
+                      
+					  <h3>Congratulations</h3><hr>
+                      
+                                             
+                       <hr>
+						</p>
+						<h3>Congrats to football!</h3>
+                    <hr>
+					<div>
+					  
+				  </div>
+                  
+                  
+                  
+				</div>
+                
+               	  
+   
+                      
+               
+
+
+		           			
+		           		</div>
+
+		           		
+
+	   					<!--facstaff navlist-->
+
+   						<div class="span3">
+
+   							<div class="well">
+
+   								<h5>Resources</h5>
+
+   								<ul class="nav nav-list">
+                                
+
+   									<li><a href="../academics/">Administration</a></li>	
+
+									<li><a href="map.php">Map</a></li>	
+
+						 		</ul>
+                                </div>
+
+
+   							
+                            <div class="well">
+                            <h5> Past Accomplishments</h5>
+                            <ul class="nav nav-list">
+                            <li><a href="2017-2018.php">2017 - 2018</a></li>
+                            <li><a href="2016-2017.php">2016 - 2017</a></li>
+                            <li><a href="2015-2016.php">2015 - 2016</a></li>
+                            <li><a href="2014-2015.php">2014 - 2015</a></li>
+                            <li><a href="2013-2014.php">2013 - 2014</a></li>
+                            <li><a href="2012-2013.php">2012 - 2013</a></li>
+                            <li><a href="2011-2012.php">2011 - 2012</a></li>
+                            <li><a href="2010-2011.php">2010 - 2011</a></li>
+                            <li><a href="2009-2010.php">2009 - 2010</a></li>
+                            <li><a href="2008-2009.php">2008 - 2009</a></li>
+                            <li><a href="200-2008.php">2007 - 2008</a></li>
+                            </ul>
+                            </div>
+   						</div>
+
+	   					<!--/facstaff navlist-->	
+
+   						
+
+					</div><!--/row-->
+
+	       			
+
+	   			</div><!--/span9 -->
+
+   			<!--/row -->
+
+<?php include("../assets/inc/footer.php") ?>
+
+
+
+    	</div> <!-- /container -->
+
+
+
+    	<!-- Yummy JavaScrips
+
+    	================================================== -->
+
+    	<!-- Placed at the end of the document so the pages load faster -->
+
+    	<script src="assets/js/jquery.js"></script>
+
+    	<script src="assets/js/bootstrap.js"></script>
+
+    	<script src="assets/js/bootstrap-carousel.js"></script>
+
+
+
+  	</body>
+
+</html>
+</html>
---
+++ gallery/17-18/1/gallerylist.php
@@ -0,0 +1,126 @@
+<?php $thisPage="gallery"; ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+
+		<title>Cedar High School</title>
+
+    	<?php include("../../../assets/inc/headtag.php") ?>
+
+  	</head>
+
+  	<body>
+
+		<?php include("../../../assets/inc/navbar.php") ?>
+
+		<style type="text/css">
+a.thumbnail { height: 102px; }
+</style>
+
+    	<!-- Let the container begin! -->
+
+		<div class="container">
+
+			<div class="row">
+
+				
+
+				<?php include("../../../assets/inc/navlist.php") ?>
+
+
+
+                <?php
+
+                    if(isset($_GET)) {
+
+                        if (isset($_GET['year']) && isset($_GET['quarter'])) {
+
+							$year = $_GET['year'];
+
+							$quarter = $_GET['quarter'];
+
+                        }else {
+
+							$year = "17-18";
+
+							$quarter = "1";
+
+                        }
+
+                    }
+
+                ?>
+
+                
+
+   			  <div class="span9">		
+
+	       			<div class="well">
+
+          				<h3>2017 Quarter 1</h3>
+
+           			</div>
+                                      
+	   			</div>
+
+	   			<!--teacher grid -->		
+
+				<div class="row span9">
+
+                	<div class="row-fluid">
+
+                    	<div class="row span9">
+
+                            <div class="well">
+
+                                Go <a href="http://chs.ironk12.org/gallery/">back</a>.
+
+                                <!--<ul class="thumbnails">
+
+                                    <li class="span2">
+
+                                        <a href="./gallerylist.php?year=1213&quarter=1" class="thumbnail">
+
+                                            <img src="../assets/img/logo.png" width="160px" height="200px" alt="">
+
+                                        </a>
+
+                                    </li>
+
+                                </ul>/-->
+
+                            </div>
+
+                        </div>
+
+                	</div>
+           		</div><!--/teacher grid --> 
+			</div><!--/row-->
+
+
+
+      		<?php include("../../../assets/inc/footer.php") ?>
+            
+        </div><!-- /container -->
+
+
+
+    	<!-- Yummy JavaScrips
+
+    	================================================== -->
+
+    	<!-- Placed at the end of the document so the pages load faster -->
+
+    	<script src="../../../assets/js/jquery.js"></script>
+
+    	<script src="../../../assets/js/bootstrap.js"></script>
+
+    	
+
+  	</body>
+
+</html>
+
